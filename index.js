0717fd144cc4ac29de7316fdf43d431add6eee03
Will Walker <williamwalker753@gmail.com>
2020-03-11 21:59:40 -0500

    revision js

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 $(function input(){
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
-        
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();      
         let name = $(event.currentTarget).find('#shopping-list-entry').val();
-        $('.shopping-list').append('<li><span class="shopping-item"><span id="newName"></span></span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-        $('#newName').html(name);
+        $('#shopping-list-entry').val('');
+        $('.shopping-list').append(`<li><span class="shopping-item">${name}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
         console.log(name);
     });
-    $('.shopping-item-delete').on('click', function(event){
-        event.stopPropagation();
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
         $(this).closest('li').remove();
-        console.log('delete');
-    });
-    $('.shopping-item-toggle').on('click', function(event){
-        //event.stopPropagation();
-        $('.shopping-item').toggleClass('shopping-item__checked').closest('.shopping-item');
-        console.log('check');
-    });
+      });
     
-        
+      $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+      });    
 });
+
 $(input());
